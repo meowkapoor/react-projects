728d7ad4be33a9f6bbf73026f067045a53f307c3
meowkapoor <2205885@kiit.ac.in>
2025-03-27 04:15:50 +0530

    completed counter project using useState() hook

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,30 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(15);
+
+  const addValue = () => {
+    count >= 20 ? setCount(20) : setCount(count + 1);
+  }
+
+  const subValue = () => {
+    count <= 0 ? setCount(0) : setCount(count - 1);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>coffee & code</h1>
+      <h2>counter value is {count} where max is 20 and min is 0</h2>
+
+      <button onClick={addValue}>Increase from {count}</button>
+      <br />
+      <button onClick={subValue}>Decrease from {count}</button>
     </>
   )
 }
